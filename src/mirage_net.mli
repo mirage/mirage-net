bb0f321d12507445776f9dcc2dd98e399a80fbd3
Pierre Alain <65669679+palainp@users.noreply.github.com>
2024-10-01 11:30:00 +0200

    use bytes instead of Bytes.t
    Co-authored-by: Hannes Mehnert <hannes@mehnert.org>

--- src/mirage_net.mli
+++ src/mirage_net.mli
@@ -57,7 +57,7 @@ module type S = sig
   (** Disconnect from the network device. While this might take some time to
       complete, it can never result in an error. *)
 
-  val write: t -> size:int -> (Bytes.t -> int) -> (unit, error) result Lwt.t
+  val write: t -> size:int -> (bytes -> int) -> (unit, error) result Lwt.t
   (** [write net ~size fill] allocates a buffer of length [size], where [size]
      must not exceed the interface maximum packet size ({!mtu} plus Ethernet
      header). The allocated buffer is zeroed and passed to the [fill] function
